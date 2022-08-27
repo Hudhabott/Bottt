e9a828f5b4ae68543b36157fb0c3d71b9e1333de
Hudhabott <112089238+Hudhabott@users.noreply.github.com>
2022-08-28 03:49:53 +0700

    Update main-thanks.js

--- plugins/main-thanks.js
+++ plugins/main-thanks.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 conn.sendImageAsSticker(m.chat, 'https://database.tioclkp02.repl.co/1240fb4a4d600fb02355b.png', m, { packname: "Sama Sama by", author: "Hudha:)" })
 }
 
-handler.customPrefix = /^(Terima Kasih|Thanks|tq)$/i
+handler.customPrefix = /^(Terima Kasih|Thanks|tq|Thnks|Makasih|thnks)$/i
 handler.command = new RegExp
 
 module.exports = handler
